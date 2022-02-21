ac9319021947ecb10895bb8b417bfed7fecf97ba
DiltaFebiana <febianadilta@gmail.com>
2022-02-21 22:27:13 +0700

    prak3No4

--- routes/web.php
+++ routes/web.php
@@ -63,21 +63,42 @@ Route::group(['prefix'=>'/category'], function () {
 });
 
 Route::get('/news', [App\Http\Controllers\NewsController::class, 'news']); 
+
+
+Route::get('/program', [App\Http\Controllers\NewsController::class, 'program']); 
 Route::group(['prefix'=>'/program'], function () {
-    Route::get('/karir', function(){
-        echo 'Ilustrator <br>';
-        echo 'Trainer Bisnis dan Pemasaran <br>';
-        echo 'Fronted Developer <br>';
-    });
-    Route::get('/magang', function(){
-        echo 'Magang Teknis <br>';
-        echo 'Magang Non-Teknis <br>';
-    });
-    Route::get('/kunjungan-industri', function(){
-        echo 'Kunjungan Industri: Tanggungjawab dan Tugas Seorang Programmer <br>';
-        echo 'Kunjungan Industri : Game, Animasi, dan Industri Kreatif';
-    });
+Route::get('/karir', function(){
+    echo 'Ilustrator <br>';
+    echo 'Digital Strategist <br>';
+    echo 'Fronted Developer <br>';
+    echo 'Pengembangan Kurikulum dan Program <br>';
+    echo 'Customer Service';
 });
+Route::get('/magang', function(){
+    echo 'Magang Teknis <br>
+    Magang dalam bidang teknis seperti Design (Game design, Web/App design, UI/UX design), 
+    Art & Illustration (Sketch, Storyboard, Character Dev, Game Assets Production, Animation Assets Production),<br> 
+    3D Creation, Video & Animation, Audio Engineering, QA & Testing dan Programming (Game, Web, Apps).
+    Kami mengalihkan semua proses pendaftaran, proses magang dan pelaporan melalui kerjasama dengan Game Lab Indonesia.<br> 
+    Jadi jika ingin magang di Educa Studio yang berhubungan dengan hal-hal teknis seperti di atas, 
+    maka silakan melakukan pendaftaran melalui situs https://www.gamelab.id <br>';
+
+    echo 'Magang Non-Teknis <br>
+    Magang non teknis meliputi Marketing (digital marketing, copywriting, content marketing, SEO, ASO, growth hacking, <br>
+    dan lain-lain), Business Development (partnership dan lain-lain), Keuangan (finance, accounting, etc), Public Relation, <br>
+     Sekretaris, Administrasi Bisnis dan PGSD (keguruan).
+    Untuk magang non teknis seperti di atas, silakan melakukan apply melalui email cs@educastudio.com dengan Subject “[Internship] Nama Program“. <br>
+    Contohnya “[internship] Public Relation”. Sertakan CV lengkap, durasi magang, motivasi magang dan profil institusi (kampus dan smk). <br>';
+});
+Route::get('/kunjungan-industri', function(){
+    echo 'Kunjungan Industri: Tanggungjawab dan Tugas Seorang Programmer <br>';
+    echo 'Kunjungan Industri : Game, Animasi, dan Industri Kreatif <br>';
+    echo 'Kunjungan Industri :Mengintip Dapur Kreatif Educa Studio <br>';
+    echo 'Kunjungan Industri SMK N 1 Talangpadang Lampung <br>';
+    echo 'Educa Studio Mendapatkan Kunjungan Industri dari PPA Anugrah Kudus';
+});
+});
+
 
 
    
