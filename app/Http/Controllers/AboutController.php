7aec5957aebb34e87ba72f93d0c82a679515aeaa
DiltaFebiana <febianadilta@gmail.com>
2022-02-21 20:10:58 +0700

    Prak2

---
+++ app/Http/Controllers/AboutController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AboutController extends Controller
+{
+    public function about(){
+        echo "NIM   : 2041720136 <br> ";
+        echo "Nama  : Dilta Febiana";
+    }
+}
