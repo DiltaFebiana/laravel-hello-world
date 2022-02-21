60702ef47ca41213c971e1efcd1d1037d5fecaf8
DiltaFebiana <febianadilta@gmail.com>
2022-02-21 19:46:21 +0700

    PageControll2

---
+++ app/Http/Controllers/PageController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PageController extends Controller
+{
+    public function index(){
+        echo "Selamat Datang";
+    }
+
+    public function about(){
+        echo "NIM   : 2041720136";
+        echo "Nama  : Dilta Febiana";
+    }
+
+    public function article($id='1'){
+        echo "Halaman artikel dengan id ".$id;
+    }
+}
