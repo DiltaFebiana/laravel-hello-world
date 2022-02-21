0ece9f114a271eeb3e04c706b155e365972daa40
DiltaFebiana <febianadilta@gmail.com>
2022-02-21 21:20:50 +0700

    revProduct2

--- routes/web.php
+++ routes/web.php
@@ -47,8 +47,7 @@ Route::group(['prefix'=>'/products'], function () {
         echo 'Marbel Premium <br>';
         echo 'Marbel Piano <br>';
         echo 'Marbel Juz Amma';
-    }
-}
+    })
     });
 
 
